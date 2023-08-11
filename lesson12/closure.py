64da5128a7b5e94c18bdca4b51a80cac5b928a34
rokib97 <hasanrokib1997@gmail.com>
2023-08-12 02:57:00 +0600

    practice modules , make our own module and call it inside other py file

--- lesson12/closure.py
+++ lesson12/closure.py
@@ -1,22 +1,24 @@
-def parent_function(person, coins):
-    # coins = 3
-    print(coins)
+def parent_function(person):
+    coins = 3
+    
     def play_game():
         nonlocal coins
         coins -= 1
         
         if coins > 1:
-            print("\n" + person + " has " + str(coins) + " coins left!") 
+            print(f"\n{person} has {coins} coins left!") 
         elif coins == 1:
-            print("\n" + person + " has " + str(coins) + " coin left!") 
+            print(f"\n{person} has {coins} coins left!") 
         else:
-            print("\n" + person + " is out of coins!") 
-    
+            print(f"\n{person} is out of coins!") 
+            
     return play_game
 
-tommy = parent_function("tommy",3)
-jenny = parent_function("jenny",5)
+tommy = parent_function("tommy")
+jenny = parent_function("jenny")
 tommy()
 tommy()
+tommy()
+jenny()
 
 
---
+++ lesson14/modules.py
@@ -0,0 +1,17 @@
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import kansan
+from rps7 import rock_paper_scissors
+print(pi)
+
+# for item in dir(rdm):
+#     print(item)
+# rdm.choice("123")
+kansan.randomfunfact()
+
+print(__name__)
+print(kansan.__name__)
+rock_paper_scissors()
+sys.exit()
